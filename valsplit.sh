8cac93d50f58fc6cdeb2203d4e4df099f022c310
Sailesh Patel <saileshapatel@gmail.com>
2017-03-27 07:45:51 +0100

    argument check

--- valsplit.sh
+++ valsplit.sh
@@ -3,3 +3,7 @@
 #Author: Sailesh Patel 
 #This program checks MIPS assembler code and outputs correct/incorrect syntax into text files
 #There are three command line arguments required
+
+if [ $# -ne 3 ]; then
+	echo "These files will not be considered."
+fi
