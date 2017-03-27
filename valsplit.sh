20c4be3e92753c98c875ddcf9a7bd66d24693b3a
Sailesh Patel <saileshapatel@gmail.com>
2017-03-27 07:45:36 +0100

    inital

---
+++ valsplit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Script name: valsplit.sh
+#Author: Sailesh Patel 
+#This program checks MIPS assembler code and outputs correct/incorrect syntax into text files
+#There are three command line arguments required
